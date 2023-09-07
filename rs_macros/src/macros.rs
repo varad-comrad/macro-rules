774b8ed4360b2b483e966f975a8d5be66a2fb739
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-09-07 17:26:08 -0300

    developed 2 macros

--- rs_macros/src/macros.rs
+++ rs_macros/src/macros.rs
@@ -1,20 +1,34 @@
-// macro_rules! comprehension{
-//     () => {
-        
-//     };
-//     ($x:ident) => {
-//         $x
-//     };
-//     ($x:ident, $($xs:ident),+) => {
-//         $x + comprehension!($($xs),+)
-//     }
-// }
-// macro_rules! map {
-//     () => {
-        
-//     };
-//     ($x:ident) => {
-//         $x
-//     };
-// }
+#[macro_export]
+macro_rules! vec_comprehension{
+    ($exp:expr, $var:ident in $cont:expr, $(if $cond:expr)?) => {
+        {
+            let mut aux = Vec::new();
+            for $var in $cont {
+                $(if $cond)? {
+                    aux.push($exp);
+                }
+            }
+            aux
+        }
+    };
+}
 
+pub use vec_comprehension;
+
+#[macro_export]
+macro_rules! map {
+    ($key:ty, $val:ty) => {
+        {
+            let aux: HashMap::<$key,$val> = HashMap::new();
+            aux
+        }
+    };
+    ($($key:expr => $val:expr), *) => {
+        {
+            let mut aux = HashMap::new();
+            $(aux.insert($key, $val);)*
+            aux
+        }
+    };
+}
+pub use map;
--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,5 +1,5 @@
-// use rs_macros;
+use rs_macros::macros;
 
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}",macros::vec_comprehension!(2*x, x in vec![1,2,3,4,6,7,7,5,45,6,1,645,4,2,5],));
 }
